7a98d5c492c8cb417b24da9a89f7b003b683318e
Vannida Lim <vannida731@gmail.com>
2019-07-30 20:25:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+ function shout(string) {
+  return string.toUpperCase() 
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout('hello'))
+}
+
+function logWhisper(string) {
+  console.log(whisper('HELLO'))
+}
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma.toLowerCase() === lowercase);
+    return "I can't hear you!"
+}
